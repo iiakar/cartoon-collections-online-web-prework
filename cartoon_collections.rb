a7d05b43e1a8f2a043219e56ff4b64dac3428cf4
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-30 14:23:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves
     print dwarves
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
+  
+  
 end
 
 def long_planeteer_calls# code an argument here
